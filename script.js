1cf73a07ad9d9e15e1e31b2e95205fcf61b71f17
islandhippy <143716620+islandhippy@users.noreply.github.com>
2023-09-23 15:45:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -66,12 +66,11 @@ function checkGuess () {
         return
     }
 
-   /* if (!WORDS.includes(guessString)) {
+  /*  if (!WORDS.includes(guessString)) {
         toastr.error("Word not in list!")
         return
-}
-*/
-
+    }
+    */
 
     
     for (let i = 0; i < 5; i++) {
